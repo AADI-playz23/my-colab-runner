9c2f940cd2085ec48be6e09bf757886806944fba
Aadarsh Pandey <aadibabaji12345@gmail.com>
2026-06-02 13:35:27 +0530

    Delete api directory

--- api/db.js
+++
@@ -1,6 +0,0 @@
-import { Pool } from '@neondatabase/serverless';
-
-// Create a single connection pool instance
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-export default pool;
